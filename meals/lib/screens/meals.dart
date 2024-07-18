8c4a7f74983da92b8f68e3b614a8a24e0d8f8f65
İsmail Efe Ataş <iefeatas@Ismail-MacBook-Air.local>
2024-07-18 17:34:55 +0300

    Meals App created

---
+++ meals/lib/screens/meals.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+import 'package:meals/screens/meal_details.dart';
+import 'package:meals/widgets/meal_item.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({super.key, this.title, required this.meals, required this.onToggleFavorite});
+
+  final String? title;
+  final List<Meal> meals;
+
+  final void Function(Meal meal) onToggleFavorite;
+
+  void selectMeal(BuildContext context,Meal meal){
+    Navigator.of(context).push(MaterialPageRoute(builder: (ctx) {
+      return MealDetailsScreen(meal: meal,onToggleFavorite: onToggleFavorite);
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = ListView.builder(
+      itemCount: meals.length,
+      itemBuilder: (context, index) {
+        return MealItem(meal: meals[index],onSelectMeal: (BuildContext context, Meal meal){
+          selectMeal(context,meal);
+        },);
+      },
+    );
+
+    if (meals.isEmpty) {
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'There is no meals available right now',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onSurface),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Try selecting different category',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onSurface),
+            ),
+          ],
+        ),
+      );
+    }
+
+    if(title == null){
+      return content;
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title!),
+      ),
+      body: content,
+    );
+  }
+}
